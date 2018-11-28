5e71f8bf2ea4c5733a4d8a4cba373c92471c7317
Tobias <tobias.reichel@hs-augsburg.de>
2018-11-28 13:22:31 +0100

    working monte carlo; Fertige Abgabe Aufgabe 6

--- Aufgabe1/MonteCarlo/PythonApplication1/PythonApplication1/monte_carlo_lcg.py
+++ Aufgabe1/MonteCarlo/PythonApplication1/PythonApplication1/monte_carlo_lcg.py
@@ -3,10 +3,13 @@ import time
 def monte_carlo():
     '''berechnet Pi'''
     hits = i = 0
-    run = 10000000
+    run = 1000000
+    gen = lcg(2**32, 22695477, 1)
+    gen2 = lcg(2**48, 25214903917 , 11)
+
     while i < run:
-        x = next(lcg(2**32, 22695477, 1))    #c values
-        y = next(lcg(2**48, 25214903917 , 11))    #java values
+        x = next(gen)    #c values
+        y = next(gen2)    #java values
         if x*x + y*y <= 1:
             hits += 1
         i += 1
@@ -15,6 +18,6 @@ def monte_carlo():
 def lcg(modulus, a, c, seed = time.time()*1000):
     while True:
         seed = (a * seed + c) % modulus
-        yield seed
+        yield seed / modulus
 
 monte_carlo()
